32795089d9e7e8cca624e678477e9e4834306768
Minh Dien <trandien04@gmail.com>
2018-07-04 11:42:19 +0700

    change

--- Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
+++ Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
@@ -1,44 +1,20 @@
-var hm = require('header-metadata');
+/*
+      Licensed Materials - Property of Five9.vn
+      © Five9 Corp. 2018
+*/
+var urlopen = require('urlopen');
+var apic = require('local:isp/policy/apim.custom.js')
+var props = apic.getPolicyProperty();
 
-// Get single header value. Read the content-type header (case insensitive).
-// Note that if there are multiple headers with the same name (e.g.
-// set-cookie), then only the first header is returned as a string
-var contentType = hm.current.get('content-type');
+var http_status_code = props.http_status_code
+var error = props.error
+var error_description = props.error_description
+var error_uri = props.error_uri
 
-// Retrieve a json object with all headers
-var allHeaders = hm.current.get();
-
-// Retrieve a json object with all headers in another form
-var allHeaders = hm.current.headers;
-
-// Set single header. Sets X-new-header to a value of new-header.
-hm.current.set('X-new-header', 'new-header');
-
- // Allows for set-cookie on multiple lines
-hm.current.set('set-cookie', ['value1', 'value2']);
-
-// write the status code with a number. Standard reasonPhrase is set to
-// the corresponding value (in this case, .OK.).
-hm.current.statusCode = "200";
-hm.current.statusCode = 200;
-
-// specify status code with a string containing the statusCode and
-// reasonPhrase separated by a single space.
-hm.current.statusCode = "200 Super-Duper";
-
-// Get the status code and reason phrase
-var currentStatusCode = hm.current.statusCode;     // 200
-var currentReasonPhrase = hm.current.reasonPhrase; // Super-Duper
-
-// Also can get the status code and reason phrase off the wire
-var originalStatusCode = hm.original.statusCode;
-var originalReasonPhrase = hm.original.reasonPhrase;
-
-session.output.write({
-    contentType: contentType,
-    allHeaders: allHeaders,
-    currentStatusCode: currentStatusCode,
-    currentReasonPhrase: currentReasonPhrase,
-    originalStatusCode: originalStatusCode,
-    originalReasonPhrase: originalReasonPhrase
-});
+var response = {
+  "error" : error,
+  "error_description" : error_description,
+  "error_uri" : error_uri
+}
+session.output.write(response);
+apic.output('application/xml');
