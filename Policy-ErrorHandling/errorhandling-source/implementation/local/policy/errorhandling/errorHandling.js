8a1eb7c5c36bfd31e40c152f3c5a7738fc56fa71
Brian Pham <hieubrianpham@gmail.com>
2018-07-04 11:02:47 +0700

    initial policy error handling

---
+++ Policy-ErrorHandling/errorhandling-source/implementation/local/policy/errorhandling/errorHandling.js
@@ -0,0 +1,20 @@
+/*
+      Licensed Materials - Property of Five9.vn
+      © Five9 Corp. 2018
+*/
+var urlopen = require('urlopen');
+var apic = require('local:isp/policy/apim.custom.js')
+var props = apic.getPolicyProperty();
+
+var http_status_code = props.http_status_code
+var error = props.error
+var error_description = props.error_description
+var error_uri = props.error_uri
+
+var response = {
+  "error" : error,
+  "error_description" : error_description,
+  "error_uri" : error_uri
+}
+session.output.write(response);
+apic.output('application/xml');
