6f96bfecdcc86de93c37b0d4fc57dbab9bca73dd
Minh Dien <trandien04@gmail.com>
2018-07-04 14:41:13 +0700

    change

--- Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
+++ Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
@@ -6,12 +6,15 @@ var urlopen = require('urlopen');
 var apic = require('local:isp/policy/apim.custom.js')
 var props = apic.getPolicyProperty();
 
+var http_status_code = props.http_status_code
+var error = props.error
+var error_description = props.error_description
+var error_uri = props.error_uri
 
 var response = {
   "error" : "error",
   "error_description" : "error_description",
   "error_uri" : "error_uri"
 }
-apic.output('application/json');
-session.output.write('{"response":"Hello World"}');
-// apim.setvariable('message.headers', "json.headers");
+apim.setvariable('message.body', {"hello" :"world"});
+apim.setvariable('message.headers', {"Content-Type":"application/json"});
