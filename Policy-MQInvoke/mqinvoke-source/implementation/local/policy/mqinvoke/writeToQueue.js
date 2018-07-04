c0bec3eb318ec25e4f49452b179008a1f8917bad
Minh Dien <trandien04@gmail.com>
2018-07-04 15:27:41 +0700

    change

--- Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
+++ Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
@@ -7,11 +7,22 @@ var apic = require('local:isp/policy/apim.custom.js')
 var props = apic.getPolicyProperty();
 
 
-var response = {
-  "error" : "error",
-  "error_description" : "error_description",
-  "error_uri" : "error_uri"
+var sitePersonel = {};
+var employees = []
+sitePersonel.employees = employees;
+console.log(sitePersonel);
+
+var firstName = "John";
+var lastName = "Smith";
+var employee = {
+  "firstName": firstName,
+  "lastName": lastName
 }
-console.log(response);
+sitePersonel.employees.push(employee);
+console.log(sitePersonel);
+
+var manager = "Jane Doe";
+sitePersonel.employees[0].manager = manager;
+console.log(sitePersonel);
 apic.output('application/json');
-session.output.write(response);
+session.output.write(sitePersonel);
