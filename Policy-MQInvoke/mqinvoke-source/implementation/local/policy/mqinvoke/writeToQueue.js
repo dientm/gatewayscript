554b3b521fb522e7b8b95db6e11fde4dcf020f8f
Minh Dien <trandien04@gmail.com>
2018-07-06 11:13:48 +0700

    change

--- Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
+++ Policy-MQInvoke/mqinvoke-source/implementation/local/policy/mqinvoke/writeToQueue.js
@@ -3,22 +3,18 @@
       © Five9 Corp. 2018
 */
 var urlopen = require('urlopen');
-var apic = require('local:isp/policy/apim.custom.js')
+var apic = require('local:isp/policy/apim.custom.js');
 var props = apic.getPolicyProperty();
-var response = {
-  "error" : "error",
-  "error_description" : "error_description",
-  "error_uri" : "error_uri"
-}
-response.readAsJSON(function(error, jsonObj) {
-  if (error) {
 
-  } else {
-    apic.output('application/json');
-    session.output.write(jsonObj);
-  }
-})
-// apic.setvariable('message.body', response);
-// console.log(response);
-// apic.output('application/json');
-// session.output.write(JSON.parse(response));
+// var http_status_code = props.http_status_code;
+// var error = props.error;
+// var error_description = props.error_description;
+// var error_uri = props.error_uri;
+
+var response = {}
+response.error = 'error'
+response.error_description = 'error_description'
+response.error_uri = "error_uri"
+
+session.output.write(response);
+apic.output('application/json');
